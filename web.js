536f11bb20c8768c122e7b0487e6f9dbf9f5245f
CornOnTheRoad <cornontheroad@gmail.com>
2013-07-14 21:11:42 +0000

    Fixed encoding

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     var fileBuf = fs.readFileSync('index.html');
     
-  response.send(fileBuf.toString(fileBuf));
+  response.send(fileBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
